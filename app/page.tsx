09164ef13a93271bbbd8f690bf3e72ceb6d89e52
novalsofyan <59356604+novalsofyan@users.noreply.github.com>
2025-07-03 19:05:46 +0700

    various fix (page, & image optimization)

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,17 @@ export default function Home() {
   return (
     <div className="main-wrapper pt-16 flex justify-center items-center h-screen box-border">
       <div className="main max-w-[1920px] flex flex-col justify-center items-center gap-4 md:flex-row md:w-[90%] md:gap-0">
-        <div className=" hero-img flex flex-col items-center md:justify-center md:w-[80%]">
-          <Image className="md:hidden" src="/img/quran-logo.svg" alt="Quran Logo Mobile" width={230} height={230} />
-          <Image className="hidden md:block lg:hidden" src="/img/quran-logo.svg" alt="Quran Logo Desktop" width={300} height={300} />
-          <Image className="hidden lg:block" src="/img/quran-logo.svg" alt="Quran Logo Desktop" width={400} height={400} />
+        <div className="hero-img flex flex-col items-center md:justify-center md:w-[80%]">
+          <Image
+            src="/img/quran-logo.svg"
+            alt="Quran Logo"
+            width={300}
+            height={300}
+            priority
+            className="w-[230px] h-[auto] md:w-[300px] md:h-[auto] lg:w-[400px] lg:h-[auto]"
+          />
           <p className="font-credit-img">
-            Logo "Quran Kareem" by{" "}
+            Logo &quot;Quran Kareem&quot; by{" "}
             <Link href="https://commons.wikimedia.org/wiki/File:Quran_Kareem_logo.png" target="_blank" rel="noopener noreferrer" className="link-text">
               محمد الحراق
             </Link>
